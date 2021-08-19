982c64cfd92f5ec2956c5d70233e45670f7853a6
Tatsiana Baldouskaya <tatsiana_baldouskaya@epam.com>
2021-08-19 21:10:58 +0300

    Add project files.

---
+++ Xamarin_android/AppInstaller.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace Xamarin_android
+{
+    public static class AppInstaller
+    {
+
+        public static void OnCreate()
+        {
+            AppCenter.Start("1f6e1663-642e-4d0f-8e39-aefde8a0817b",
+                   typeof(Analytics), typeof(Crashes));
+        }
+    }
+
+}
---
+++ Xamarin_android/Tests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.Enums;
+using System;
+
+namespace Xamarin_android
+{
+    [TestFixture]
+    public class Tests
+    {
+        private string newApp = @"C:/Users/Tatsiana_Baldouskaya/source/repos/Xamarin_android/onliner.apk";
+
+
+        [Test]
+        public void CloudTest()
+        {
+            AppInstaller.OnCreate();
+        }
+    }
+}
